9132a4258d4a1545f19b6af90e6480c449aef81f
CODEman <lalitsainiak23@gmail.com>
2026-01-08 17:43:10 +0530

    update cpp codes/hash_table.cpp

--- cpp codes/hash_table.cpp
+++ cpp codes/hash_table.cpp
@@ -1,27 +1,52 @@
 #include<iostream>
+#include<map>
 #include<unordered_map>
 using namespace std;
 
 
 int main(){
+  //creating hash table
+  unordered_map<string,int>m;
+
+  //method 1 to inserte
+  pair<string,int> pair2("lalit",34);
+  m.insert(pair2);
+  //method 2 to insert
+  m.insert({"jhonny",44});
+
+  //display  
+  cout<<m.at("lalit")<<endl;//at function of unordered map
+  cout<<m["jhonny"]<<endl;
+  cout<<m["unknown"]<<endl;
   
-  // creation of hash table
-  unordered_map<string, int>m;
-  //insertation 
+  //size function
+  cout<<m.size()<<endl;//size function of unorder map to check size of hash table
   
-  //method 1
-  m.insert({"lalit",34});
+  //persence function
+  cout<<"persence:"<<m.count("lalit")<<endl;//cout function of unordered map to check persence of any key. it return zero for false and return 1 for true
+  cout<<"persence:"<<m.count("bro")<<endl;
   
-  //method 2
-  pair<string,int> pair1("jhonny",44);//template method of pair  
+  //erase function
+  m.erase("jhonny");//erase function of unorder map to remove any element 
+  cout<<"after erase one element:"<<m.size()<<endl;
   
-  //display
-  cout<<m["lalit"]<<endl;
-  cout<<m["jhonny"]
+  //iterator
   
+  cout<<"while loop iterator:"<<endl;
+  //itration with while loop
+  unordered_map<string,int> :: iterator it =m.begin();
+  while(it != m.end()){
+    cout<<it->first<<" "<<it->second<<endl;
+    it++;
+  }
+  
+  cout<<"for loop iterator:"<<endl;
+  //itration with for loop
+  unordered_map<string,int> :: iterator ik;
+  for(ik =m.begin();ik!=m.end();ik++){
+    cout<<ik->first<<" "<<ik->second<<endl;
+  }
   
   
   return 0;
 }
-
-
