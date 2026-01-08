c518eb5007871ed2cbe99a3357fe705d33cccdfc
CODEman <lalitsainiak23@gmail.com>
2026-01-08 16:04:22 +0530

    update cpp codes/hash_table.cpp

--- cpp codes/hash_table.cpp
+++ cpp codes/hash_table.cpp
@@ -5,11 +5,19 @@ using namespace std;
 
 int main(){
   
+  // creation of hash table
   unordered_map<string, int>m;
+  //insertation 
+  
+  //method 1
   m.insert({"lalit",34});
   
+  //method 2
+  pair<string,int> pair1("jhonny",44);//template method of pair  
   
+  //display
   cout<<m["lalit"]<<endl;
+  cout<<m["jhonny"]
   
   
   
