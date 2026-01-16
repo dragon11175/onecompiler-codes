67a735e68753e4dac2305f536a09a3798b2b2170
CODEman <lalitsainiak23@gmail.com>
2026-01-16 12:22:32 +0530

    update javascript codes/DOM javascript/DOM_selectors.js

--- javascript codes/DOM javascript/DOM_selectors.js
+++ javascript codes/DOM javascript/DOM_selectors.js
@@ -9,5 +9,11 @@ console.log(greeting.textContent);
 //getElementsByClassName():
 
 const item=document.getElementsByClassName("item");
+console.log("getElementsByClassName returns html collection:";)
 console.log(item);
+
+console.log("now we access html collection elements by index numbers";
+console.log(item[0]);
+console.log(item[1]);
+console.log(item[2]);
 
