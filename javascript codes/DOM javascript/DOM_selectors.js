1a76978c2be7085346602e312b75767b04d65c06
CODEman <lalitsainiak23@gmail.com>
2026-01-16 12:13:55 +0530

    update javascript codes/DOM javascript/DOM_selectors.js

--- javascript codes/DOM javascript/DOM_selectors.js
+++ javascript codes/DOM javascript/DOM_selectors.js
@@ -1,6 +1,13 @@
+// DOM selectors to access elements of html webpage
 
 
 //getElementById():
 
 const greeting=document.getElementById("greeting");
 console.log(greeting.textContent);
+
+//getElementsByClassName():
+
+const item=documemt.getElementsByClassName("item");
+console.log(item.textContent);
+
