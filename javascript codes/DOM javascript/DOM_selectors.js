10ca22dd90199bedb06cf7afa254e3abca2ddd9d
CODEman <lalitsainiak23@gmail.com>
2026-01-16 15:04:35 +0530

    update javascript codes/DOM javascript/DOM_selectors.js

--- javascript codes/DOM javascript/DOM_selectors.js
+++ javascript codes/DOM javascript/DOM_selectors.js
@@ -19,14 +19,24 @@ console.log(item[2].textContent);
 
 //getElementByTagName()
 
-const paragraphs=document.getElementsByTagName("p");
+const paragraphs=document.getElementsByTagName("h2");
 console.log("getelementsByTagname returns a html collection:");
 console.log(paragraphs);
 
 console.log("now we access html collection elements by index numbers");
 console.log(paragraphs[0].textContent);
-console.log(paragraphs[1].textcontent);
+console.log(paragraphs[1].textContent);
 console.log(paragraphs[2].textContent);
 
+//queryselector()
 
+const speacial=document.querySelector(".container");
+console.log(speacial.textContent);
+
+//querySelectorAll():
+
+const speacials=document.querySelectorAll("itme-menu");
+console.log(speacials);
+// here we use for each loop for acess all elements of node 
+speacials.forEach((item)=>{console.log(item.textContent);})
 
