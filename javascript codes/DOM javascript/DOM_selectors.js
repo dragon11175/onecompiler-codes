4fa620115aa4223d288e6334ffb9078e2b598e07
CODEman <lalitsainiak23@gmail.com>
2026-01-19 09:52:09 +0530

    update javascript codes/DOM javascript/DOM_selectors.js

--- javascript codes/DOM javascript/DOM_selectors.js
+++ javascript codes/DOM javascript/DOM_selectors.js
@@ -34,7 +34,7 @@ const speacial=document.querySelector(".container");
 console.log(speacial.textContent);
 
 //querySelectorAll():
-cobsole.log("queryselectorAll");
+console.log("queryselectorAll");
 const speacials=document.querySelectorAll(".itme-menu");
 console.log(speacials);
 // here we use for each loop for acess all elements of node 
