8f01e4806021f89c6b315404502ba60abd72f406
CODEman <lalitsainiak23@gmail.com>
2026-01-19 12:32:06 +0530

    update javascript codes/DOM javascript/modifying_add_remove_elements_DOM/DOM_manipulation.js

--- javascript codes/DOM javascript/modifying_add_remove_elements_DOM/DOM_manipulation.js
+++ javascript codes/DOM javascript/modifying_add_remove_elements_DOM/DOM_manipulation.js
@@ -10,4 +10,23 @@ meassage.innerHTML+="<p>pankaj</p>";// here we add new element
 
 //updating attribute
 const image=document.getElementById("image");
-image.setAttribute("alt","old photo");//here we change image alt
+image.setAttribute("alt","old pic");
+console.log(image.getAttribute("alt"));//to check alt
+
+
+//manipulation styles
+const styled=document.getElementById("styled");
+styled.style.color="blue";
+styled.style.fontSize="48px";
+
+
+//adding/removing classes
+const box=document.getElementById("box");
+box.classList.add("shadow");//here add shadow with red-box
+box.classList.remove("red-box");//here emove red-box and remain only shadow
+box.classList.toggle("highlilghts");//add if this class not exist and remove if exists
+box.classList.toggle("highlilghts");
+console.log(box.getAttribute("class"));
+
+
+
