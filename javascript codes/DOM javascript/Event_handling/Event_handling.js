011e04e63b8a02b983709a701b3cc49137ba336b
CODEman <lalitsainiak23@gmail.com>
2026-01-20 14:12:07 +0530

    update javascript codes/DOM javascript/Event_handling/Event_handling.js

--- javascript codes/DOM javascript/Event_handling/Event_handling.js
+++ javascript codes/DOM javascript/Event_handling/Event_handling.js
@@ -2,6 +2,6 @@ const button=document.getElementById("mybutton");
 button.addEventListener("click",()=>{
    const content=document.getElementById("paragraph");
    content.textContent="clicked";
-  
+   content.style.color="red";
  
 });
