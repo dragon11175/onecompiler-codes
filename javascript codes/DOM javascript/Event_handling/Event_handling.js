6ec11290f2d632a71288facef9a3b0430166202d
CODEman <lalitsainiak23@gmail.com>
2026-01-20 14:06:24 +0530

    update javascript codes/DOM javascript/Event_handling/Event_handling.js

---
+++ javascript codes/DOM javascript/Event_handling/Event_handling.js
@@ -0,0 +1,7 @@
+const button=document.getElementById("mybutton");
+button.addEventListener("click",()=>{
+   const content=document.getElementById("paragraph");
+   content.textContent="clicked";
+  
+ 
+});
