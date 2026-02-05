9d0427144a60b4aafe73cdb7610522862062023a
CODEman <lalitsainiak23@gmail.com>
2026-02-05 09:50:43 +0530

    update JAVA_programs/Input_from_scanner.java

---
+++ JAVA_programs/Input_from_scanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class sample{
+  public static void main(String[] args){
+    
+  Scanner input=new Scanner(System.in);
+  System.out.println("please enter your name");
+  String name=input.nextLine();System.out.println("hello:"+name);
+  System.out.println("now tell me your age:");
+  int age=input.nextInt();
+  System.out.println("okk your age is:"+age);
+   
+  
+    
+  }
+}
