6d971fa0dade2ab7c3878926da23e4435dab708d
CODEman <lalitsainiak23@gmail.com>
2026-02-07 12:04:23 +0530

    update JAVA_programs/Arthmathic_opeartions.java

---
+++ JAVA_programs/Arthmathic_opeartions.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Arthmathic_operations{
+  public static void main(String[] args){
+    
+  Scanner input=new Scanner(System.in);
+  System.out.print("Enter your first number:");
+  int a=input.nextInt();
+  System.out.print("Enter your Second number:");
+  int b=input.nextInt();
+  System.out.print("Addition of your 2 numbers:");
+  System.out.println(a+b);
+  System.out.print("Subtraction of your 2 numbers:");
+  System.out.println(a-b);
+  System.out.print("Product of your 2 numbers:");
+  System.out.println(a*b);
+  System.out.print("Divide of your 2 numbers:");
+  float c=a/b;
+  System.out.println(c);
+  
+  
+    
+  }
+}
