b0c646b8969a47ed45b1f50f6984e06927bf31d9
CODEman <lalitsainiak23@gmail.com>
2026-03-18 15:21:48 +0530

    update JAVA_programs/operatoroverloading.java

---
+++ JAVA_programs/operatoroverloading.java
@@ -0,0 +1,16 @@
+class operatoroverloading{
+  
+   public static void add(int a,int b){
+     System.out.print(a*b);
+   }
+   
+   public static void add(double a,double b){
+     System.out.print(a*b);
+   }
+  
+   static public void main(String[] args){
+    
+     add(3.4,6.6);
+  }
+  
+}
