3aaf042dece6d4fde9c93180fe22fb8ded29a060
CODEman <lalitsainiak23@gmail.com>
2026-03-18 14:22:00 +0530

    update JAVA_programs/class_and_object.java

---
+++ JAVA_programs/class_and_object.java
@@ -0,0 +1,14 @@
+class sample{
+  
+  static public class rahul{
+    int a=45;
+  }
+  
+   static public void main(String[] args){
+    
+    rahul obj1 = new rahul();
+    
+    System.out.print(obj1.a);
+  }
+  
+}
