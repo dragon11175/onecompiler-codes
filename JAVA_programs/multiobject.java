ec1eab94b2a209c9f3c0499f6df0b71282d8b31c
CODEman <lalitsainiak23@gmail.com>
2026-03-23 10:25:13 +0530

    update JAVA_programs/multiobject.java

---
+++ JAVA_programs/multiobject.java
@@ -0,0 +1,18 @@
+class multiobject{
+  
+   int x;
+  
+   static public void main(String[] args){
+    multiobject obj1=new multiobject();
+    multiobject obj2=new multiobject();
+    
+    
+    obj1.x=34;
+    obj2.x=56;
+    
+    System.out.println(obj1.x);
+    System.out.println(obj2.x);
+   
+     }
+  
+}
